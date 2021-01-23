16e83fcf3c00fbf06a87b786571e5afe00aae5c7
Otavio Landim <francisco.conrado@zup.com.br>
2021-01-23 17:14:33 -0300

    add test

--- __tests__/login-post.js
+++ __tests__/login-post.js
@@ -1,14 +1,22 @@
 const request = require("supertest")
 const expect = require("chai").expect
 const URL = "https://reqres.in/"
-const endpoint = "api/users?page=2"
 
 describe('Login', () => {
   it('should do to log in', (done) => {
     request(URL)
-      .get(endpoint)
+      .get("api/users?page=2")
     .end((err, res) => {
       expect(res.status).to.be.eql(200);
+      done()
+    })  
+  })
+
+  it('should do to log in', (done) => {
+    request(URL)
+      .get("api/users/2")
+    .end((err, res) => {
+      expect(res.status).to.be.eql(400);
       done()
     })  
   })
